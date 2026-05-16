538041303814dd136f270ed31c8d74c6d171b009
Rkt <tarek.itim08@gmail.com>
2026-05-16 21:37:19 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-// Fonction pour basculer le thème
 function changerTheme() {
-    // On récupère la balise <link> du CSS grâce à son ID
     var themeLink = document.getElementById("theme-css");
     var themeActuel = themeLink.getAttribute("href");
 
     var nouveauTheme = "";
     var valeurCookie = "";
 
-    // Si on est en clair, on passe en sombre, et inversement
+    //Si on est en lightmode on switch en darkmode...
     if (themeActuel === "style.css") {
         nouveauTheme = "style_sombre.css";
         valeurCookie = "sombre";
@@ -16,10 +14,9 @@ function changerTheme() {
         valeurCookie = "clair";
     }
 
-    // Changement du fichier CSS dans le DOM (sans rechargement de page)
     themeLink.setAttribute("href", nouveauTheme);
 
-    // Sauvegarde du choix dans un cookie valable 30 jours (en secondes)
+    //La sauvegarde dans un cookie dure 30j
     var dateExpiration = new Date();
     dateExpiration.setTime(dateExpiration.getTime() + (30 * 24 * 60 * 60 * 1000));
     document.cookie = "theme=" + valeurCookie + "; expires=" + dateExpiration.toUTCString() + "; path=/";
